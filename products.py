22d2dec64a8b8f370a48dcdebcebb409e93bd370
April Tseng <aprilmow1018129@gmail.com>
2019-03-21 19:31:26 +0800

    add products.py

---
+++ products.py
@@ -0,0 +1,13 @@
+products = []
+while True:
+	name = input('please enter product name:')
+	if name == 'q':
+		break
+	price = input('please enter product price:')
+	p = []
+	p.append(name)
+	p.append(price)
+	#p = [name, price]
+	products.append(p)
+	#products.append([name, price])
+print(products)
