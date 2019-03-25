37e71a3241b89e0e8d4e001067fb00ee2c0d028f
April Tseng <aprilmow1018129@gmail.com>
2019-03-25 18:19:27 +0800

    add header

--- products.py
+++ products.py
@@ -14,3 +14,8 @@ print(products)
 
 for p in products:
 	print(p[0], 'price is', p[1])
+
+with open('products.csv', 'w') as f:
+	f.write('product,price\n') #加入標題
+	for p in products:
+		f.write(p[0] + ',' + p[1] + '\n')
